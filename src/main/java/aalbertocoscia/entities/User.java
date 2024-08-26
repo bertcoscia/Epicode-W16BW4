2a677b56b3d1fcb5750baa599baadec760cf811d
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-26 15:44:01 +0200

    adds enums, class User

---
+++ src/main/java/aalbertocoscia/entities/User.java
@@ -0,0 +1,67 @@
+package aalbertocoscia.entities;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID idUser;
+    private String nome;
+    private String cognome;
+    @Column(name = "data_di_nascita")
+    private LocalDate dataNascita;
+
+    public User() {
+    }
+
+    public User(String nome, String cognome, LocalDate dataNascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", dataNascita=" + dataNascita +
+                '}';
+    }
+}
