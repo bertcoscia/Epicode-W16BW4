cc7daa0148c9fe1017ac4993afc987aed62b769f
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-26 11:30:18 +0200

    connects repo to db

--- src/main/java/aalbertocoscia/Application.java
+++ src/main/java/aalbertocoscia/Application.java
@@ -1,6 +1,10 @@
 package aalbertocoscia;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("Epicode-W16BW4");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
