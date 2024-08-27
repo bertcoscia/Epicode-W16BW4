108e4b4c1756a503320dca019b39bff8f3702c97
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-27 09:52:56 +0200

    Update Application.java

--- src/main/java/aalbertocoscia/Application.java
+++ src/main/java/aalbertocoscia/Application.java
@@ -5,8 +5,6 @@ import aalbertocoscia.dao.UserDAO;
 import aalbertocoscia.dao.VenditoreDAO;
 import aalbertocoscia.entities.DistributoreAutomatico;
 import aalbertocoscia.entities.Rivenditore;
-import aalbertocoscia.entities.Tessera;
-import aalbertocoscia.entities.User;
 import aalbertocoscia.enums.StatoDistributoreAutomatico;
 import com.github.javafaker.Faker;
 import jakarta.persistence.EntityManager;
@@ -26,18 +24,24 @@ public class Application {
         VenditoreDAO vd = new VenditoreDAO(em);
 
 
-        User sergioMattarella = new User("Sergio", "Mattarella", "1938-05-12");
-        User user2 = new User(faker.dune().character(), faker.name().lastName(), "1998-04-15");
-        User sergioMattarellaFromDb = ud.findUserById("e8161585-b715-4be9-b59c-024c6205918b");
-        User user2FromDb = ud.findUserById("d6521de9-054e-457e-9fc4-436346feec48");
+        //User sergioMattarella = new User("Sergio", "Mattarella", "1938-05-12");
+        //User user2 = new User(faker.dune().character(), faker.name().lastName(), "1998-04-15");
+        //ud.save(sergioMattarella);
+        //ud.save(user2);
+        //User sergioMattarellaFromDb = ud.findUserById("e8161585-b715-4be9-b59c-024c6205918b");
+        //User user2FromDb = ud.findUserById("d6521de9-054e-457e-9fc4-436346feec48");
 
-        Tessera tes1 = new Tessera("2024-01-01", sergioMattarellaFromDb);
-        Tessera tes2 = new Tessera("2024-08-21", user2FromDb);
+        //Tessera tes1 = new Tessera("2024-01-01", sergioMattarellaFromDb);
+        //Tessera tes2 = new Tessera("2024-08-21", user2FromDb);
 
         DistributoreAutomatico dist1 = new DistributoreAutomatico(faker.address().fullAddress(), StatoDistributoreAutomatico.ATTIVO);
         DistributoreAutomatico dist2 = new DistributoreAutomatico(faker.address().fullAddress(), StatoDistributoreAutomatico.FUORI_SERVIZIO);
         Rivenditore riv1 = new Rivenditore(faker.address().fullAddress(), faker.company().name());
-        
+
+        //vd.save(dist1);
+        //vd.save(dist2);
+        //vd.save(riv1);
+
 
         em.close();
         emf.close();
